8c39d627cddb82054239051be8b2e37d61a8e63b
martingega <martin.gega@studenti.unimi.it>
2024-01-18 12:14:51 +0100

    inserita banale non conversione di scale come NULL object

--- src/main/java/it/unimi/di/sweng/temperature/presenter/ScaleStrategy.java
+++ src/main/java/it/unimi/di/sweng/temperature/presenter/ScaleStrategy.java
@@ -1,6 +1,12 @@
 package it.unimi.di.sweng.temperature.presenter;
 
 public interface ScaleStrategy {
+
+  ScaleStrategy NO_SCALE = new ScaleStrategy() {
+    @Override public double convertFromCelsius(double temperature) { return temperature; }
+    @Override public double convertToCelsius(double temperature) { return temperature; }
+  };
+
   double convertFromCelsius(double temperature);
 
   double convertToCelsius(double temperature);
